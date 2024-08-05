6625890305dabc89a693a105cb9dce8c47891329
tenthid <tenthsut@gmail.com>
2024-08-05 01:44:36 +0000

    just commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,27 @@
+@php
+    $content = [
+        [
+            'title' => 'anggrek mekar pontianak',
+            'url' => 'https://cdn.videy.co/94DGK4lx.mp4'
+        ],
+        [
+            'title' => 'kartel brutal',
+            'url' => 'https://cdn.videy.co/w1BOxGUg.mp4'
+        ]
+    ];
+
+    $lastIndexContent = count($content) - 1;
+    $randContent = mt_rand(0, $lastIndexContent); 
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anggrek mekar pontianak</title>
+    <title>{{ $content[$randContent]['title'] }}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="icon" href="https://cdn.discordapp.com/attachments/715603485101522984/1268473531889619005/Screenshot_2023-11-19_173942.png?ex=66ad3649&is=66abe4c9&hm=97e459739ae4afac5da10ae9ae38946945a08c7e68b47f821efe3f2f1d94ff61&">
 
     <style>
         .container-modal {
@@ -17,13 +34,9 @@
             height: 100vh;
             top: 0; 
             z-index: 99;
-            transition: display 0.5s ease-in-out;
+            transition: 1s ease-in-out;
         }
-
-        .hide {
-            display: none;
-        }
-
+        
         .modal {
             color: white;
             display: flex;
@@ -36,12 +49,27 @@
             padding: 20px;
             height: fit-content;
         }
-
+        
         .btn {
             background-color: black;
             color: white;
             border-radius: 10px;
             padding: 6px 12px;
+        }
+        
+        .thumbnail {
+            display: flex; 
+            position: absolute; 
+            z-index: 2; 
+            width: 100%; 
+            height: 100%; 
+            background-color:rgba(0, 0, 0, 0.70); 
+            justify-content: center;
+            align-items: center;
+        }
+
+        .hide {
+            display: none;
         }
     </style>
 </head>
@@ -49,10 +77,13 @@
 
     <div style="display: flex; min-height: 100vh; max-height: fit-content; justify-content: center; align-items: center; background-color: black; flex-direction: column;">
         <h1 style="color: white;">Ngapain hayoo</h1>
-        <div style="height: 60%; width: 80%; justify-content: center; display: flex; align-items: center; position: relative;">
-            <img id="thumbnail" onclick="thumbnailHide()" style="position: absolute; z-index: 2;" width="40%" height="auto" src="https://cdn.discordapp.com/attachments/715603485101522984/1268473531889619005/Screenshot_2023-11-19_173942.png?ex=66ad3649&is=66abe4c9&hm=97e459739ae4afac5da10ae9ae38946945a08c7e68b47f821efe3f2f1d94ff61&" alt="thumbnail">
-            <video width="100%" height="100%" controls autoplay>
-                <source src="{{ url('https://cdn.videy.co/w1BOxGUg.mp4') }}" type="video/mp4" >
+        <div style="height: 60%; width: 80%; position: relative;">
+            <div id="thumbnail" onclick="thumbnailHide()" class="thumbnail">
+                <img  width="40%" height="auto" src="https://cdn.discordapp.com/attachments/715603485101522984/1268473531889619005/Screenshot_2023-11-19_173942.png?ex=66ad3649&is=66abe4c9&hm=97e459739ae4afac5da10ae9ae38946945a08c7e68b47f821efe3f2f1d94ff61&" alt="thumbnail">
+            </div>
+            <video width="100%" height="100%" controls id="video">
+                <source src="{{ $content[$randContent]['url'] }}" type="video/mp4">
+                dih lu pake browser paan?, tag video ga support di browser lu.
             </video>
             <p style="display:none">
                 ngapain inspect hayoooo???
@@ -70,6 +101,7 @@
     <script>
         let modalContainer = document.getElementById('modal');
         let thumbnail = document.getElementById('thumbnail');
+        let video = document.getElementById('video');
 
         const modalHide = () => {
             modalContainer.classList.add('hide');
@@ -77,6 +109,7 @@
 
         const thumbnailHide = () => {
             thumbnail.classList.add('hide');
+            video.play();
         }
     </script>
 </body>
